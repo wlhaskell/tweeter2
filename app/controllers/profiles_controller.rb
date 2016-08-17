4d50fa9087e29a8a925b1cecbfaa3ee042e7a986
Will Haskell <wlhaskell@gmail.com>
2016-08-16 23:40:33 -0400

    fixed update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -37,8 +37,8 @@ class ProfilesController < ApplicationController
 
   def update
     respond_to do |format|
-      if @profile.update(twit_params)
-        format.html { redirect_to @profile, notice: 'Twit was successfully updated.' }
+      if @profile.update(profile_params)
+        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
         format.json { render :show, status: :ok, location: @profile }
       else
         format.html { render :edit }
