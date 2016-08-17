244eea0684f1a5e588f37c81b990e84c933895b9
Will Haskell <wlhaskell@gmail.com>
2016-08-16 23:33:03 -0400

    moved jquery.jcrop around

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require jquery.jcrop
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
